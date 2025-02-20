58da53082021b64b60e66d9d2920bf19b1581329
Joshtri <stuffofyos1516@gmail.com>
2025-02-20 21:37:49 +0700

    Update keywords in metadata for improved SEO

--- app/(public)/toko-buku-online/layout.js
+++ app/(public)/toko-buku-online/layout.js
@@ -2,7 +2,7 @@ export const metadata = {
   title: "Belanja Buku Online | Toko Buku Yasipikan",
   description:
     "Temukan berbagai buku berkualitas di toko kami dengan harga terbaik. Mulai dari buku pelajaran, novel, hingga referensi akademik tersedia dengan mudah.",
-  keywords: ["yasipikan toko buku", "yayasan sirih pinang kebaikan toko buku"],
+  keywords: ["yasipikan toko buku", "yayasan sirih pinang kebaikan toko buku", "toko buku yasipikan", "belanja buku online yasipikan", "toko buku online yasipikan"],
 };
 
 export default function Layout({ children }) {
