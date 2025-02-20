810deb0aeeb41f4d2c3a99f27ce838a8ec8924f1
Joshtri <stuffofyos1516@gmail.com>
2025-02-20 21:41:07 +0700

    Add layout.js with metadata for Daftar Penulis Buku page

---
+++ app/(public)/daftar-penulis-buku/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: "Daftar Penulis Buku | Yasipikan Publisher",
+    description:
+      "Jelajahi daftar penulis buku yang tergabung dalam Yasipikan Publisher. Temukan karya-karya terbaik serta berbagai informasi mengenai penulis favorit Anda.",
+    keywords: ["penulis buku", "yasipikan publisher", "daftar penulis", "daftar penulis yasipikan" , "buku yasipikan"],
+};
+
+export default function layout({children}) {
+ 
+    return <>{children}</>;
+   
+}
